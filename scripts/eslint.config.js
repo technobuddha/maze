1d2c8aec582b5d5d3e78524a7d2a0ecfcccd8a81
Phil Hill <phil@technobuddha.com>
2026-08-28 06:30:12 -0400

    project

--- scripts/eslint.config.js
+++
@@ -1,7 +0,0 @@
-// 🚨
-// 🚨 CHANGES TO THIS FILE WILL BE OVERRIDDEN
-// 🚨
-// @ts-check
-import { lint } from '@technobuddha/project';
-
-export default lint({ files: ['*.ts'], platform: 'node', typescript: true });
